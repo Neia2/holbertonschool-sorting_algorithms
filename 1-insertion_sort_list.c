9c9ec4136c858774c5b0c5ba023c48fa29d748dd
Neia2 <nenaslife@gmail.com>
2024-04-12 16:02:27 +0200

    fixing inserction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list -  sorts a DLL of integers using the Insertion sort
  * @list: DLL to sort
@@ -7,35 +6,44 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	int done = 0;
+	listint_t *tmp_node, *aux_node;
 
-    listint_t *tmp_node, *aux_node;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    tmp_node = (*list)->next;
-    while (tmp_node != NULL)
-    {
-        aux_node = tmp_node;
-        tmp_node = tmp_node->next;
-
-        while (aux_node->prev != NULL && aux_node->n < aux_node->prev->n)
-        {
-            aux_node->prev->next = aux_node->next;
-
-            if (aux_node->next != NULL)
-                aux_node->next->prev = aux_node->prev;
-
-            aux_node->next = aux_node->prev;
-            aux_node->prev = aux_node->prev->prev;
-
-            if (aux_node->prev == NULL)
-                *list = aux_node;
-            else
-                aux_node->prev->next = aux_node;
-
-            aux_node->next->prev = aux_node;
-
-            print_list(*list);
-        }
-    }
+	tmp_node = (*list);
+	aux_node = tmp_node->next;
+	while (aux_node)
+	{
+		while (tmp_node)
+		{
+			if (aux_node->n < tmp_node->n)
+			{
+				if (aux_node->next)
+					aux_node->next->prev = aux_node->prev;
+				else
+					done = 1;
+				tmp_node->next = aux_node->next;
+				aux_node->next = tmp_node;
+				aux_node->prev = tmp_node->prev;
+				if (tmp_node->prev)
+					tmp_node->prev->next = aux_node;
+				else
+					*list = aux_node;
+				tmp_node->prev = aux_node;
+				tmp_node = aux_node->prev;
+				print_list(*list);
+			}
+			else
+			{
+				aux_node = aux_node->next;
+				tmp_node = tmp_node->next;
+			}
+		}
+		if (done)
+			break;
+		aux_node = aux_node->next;
+		tmp_node = aux_node->prev;
+	}
 }
